fed49dae40ad518b263fb65f6af2b9ebb97ecde9
Matthew Astley <code$mca@t8o.org>
2015-06-14 22:26:40 +0100

    fight the borrow checker (lose)

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ use std::io::BufRead;
 fn main() {
     let mut args = env::args();
     let prog = args.next().expect("no $0 ?");
-    let infile = args.next().expect(& format!("Syntax: {} <input.md>", prog));
+    let infile = args.next().expect(& format!("Syntax: {} <input.md> [ <outdir> ]", prog));
+    let outdir = match args.next() {
+        Some(dir) => dir.as_str().as_ref(),
+        None => "./",
+    };
+
     let inpath = Path::new(&infile);
-    println!("hopefully {:?} is my file", inpath);
+    println!("hopefully {:?} is my file; write to {}", inpath, outdir);
 
     // thanks A.B. in http://stackoverflow.com/a/25168563
     let infh = BufReader::new(File::open(&inpath).unwrap());
     for line in infh.lines().filter_map(|result| result.ok()) {
-        println!("{:?}", line);
+        println!("{}", line);
     }
 }
