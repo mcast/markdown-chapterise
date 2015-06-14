c53303589b1344d054efd7129caaabf941bcd61d
Matthew Astley <code$mca@t8o.org>
2015-06-14 21:01:21 +0100

    reading text by lines

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::path::Path;
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
 
 fn main() {
     let mut args = env::args();
     let prog = args.next().expect("no $0 ?");
     let infile = args.next().expect(& format!("Syntax: {} <input.md>", prog));
     let inpath = Path::new(&infile);
-    println!("hopefully {:?} is my file", inpath)
+    println!("hopefully {:?} is my file", inpath);
+
+    // thanks A.B. in http://stackoverflow.com/a/25168563
+    let infh = BufReader::new(File::open(&inpath).unwrap());
+    for line in infh.lines().filter_map(|result| result.ok()) {
+        println!("{:?}", line);
+    }
 }
