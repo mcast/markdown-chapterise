f9bd1afd09b29910b693bde81f6fa172be582bae
Matthew Astley <code$mca@t8o.org>
2015-06-18 09:16:56 +0100

    fix CamelWarnings

--- src/mdslurp.rs
+++ src/mdslurp.rs
@@ -8,18 +8,18 @@ pub enum MarkdownEle {
 impl MarkdownEle {
     pub fn new(line: String, next: Option<&String>) -> MarkdownEle {
         let ch: Vec<char> = line.chars().collect();
-        let mut hdrLevel = 0;
+        let mut hdr_level = 0;
         for ch in line.chars() {
             match ch {
-                '#' => hdrLevel += 1,
+                '#' => hdr_level += 1,
                 ' ' => break,
                 _ => {
-                    hdrLevel = 0;
+                    hdr_level = 0;
                     break
                 },
             }
         }
-        match hdrLevel {
+        match hdr_level {
             0 => MarkdownEle::Other { txt: line },
             n => MarkdownEle::Head { txt: line, n: n },
         }
